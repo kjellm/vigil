b76f6955866491eaa1007db4deba4f7f0874e7f6
Kjell-Magne Øierud <kjellm@oierud.net>
2013-02-28 20:24:37 +0100

    Refactor: Extract method #_build_basebox

--- lib/vigil.rb
+++ lib/vigil.rb
@@ -53,36 +53,28 @@ class Vigil
       @rebuild = false
     end
 
-    def _create_required_directories
-      @x.mkdir_p @run_dir_revision
-      @x.mkdir_p @run_dir_boxes
-    end
-
     def run
       _create_required_directories
       @x.chdir @run_dir_revision
       
       #TODO use grit for git stuff
       
-      @x._system "git clone #{@project_dir} ."
+      @x._system "git clone #@project_dir ."
       @x._system "git checkout vigil"  #FIXME
       
       @x._system "ln -s #{File.expand_path(File.join(@run_dir, 'iso'))}"
   
-      unless @x.exists?(File.join @run_dir_boxes, "#{@project}-#{@revision_id}_complete.pkg")
-        previous_revision_box_name = File.join @run_dir_boxes, "#{@project}-#{@revision_id.to_i - 1}.box"
-        current_revision_box_name = File.join @run_dir_boxes, "#{@project}-#{@revision_id}.box"
+      unless @x.exists?(File.join @run_dir_boxes, "#@project-#{@revision_id}_complete.pkg")
+        previous_revision_box_name = File.join @run_dir_boxes, "#@project-#{@revision_id.to_i - 1}.box"
+        current_revision_box_name = File.join @run_dir_boxes, "#@project-#@revision_id.box"
         if @x.exists?(current_revision_box_name)
-        elsif !@x.exists?(previous_revision_box_name) or
-            !@x.__system "git diff --quiet HEAD^ -- definitions" #FIXME
-          @x._system "vagrant basebox build --force --nogui '#{@project}'"
-          @x._system "vagrant basebox validate '#{@project}'"
-          @x._system "vagrant basebox export '#{@project}'"
-          @x._system "mv #{@project}.box #{@run_dir_boxes}/#{@project}-#{@revision_id}.box"
-          @x._system "vagrant basebox destroy #{@project}" #FIXME put in ensure block
-          @rebuild = true
+          # noop
+        elsif @x.exists?(previous_revision_box_name) and
+            @x.__system "git diff --quiet HEAD^ -- definitions" #FIXME
+          @x._system "ln #{previous_revision_box_name} #{current_revision_box_name}"
         else
-          @x._system "ln #{previous_revision_box_name} #{@run_dir_boxes}/#{@project}-#{@revision_id}.box"
+          _build_basebox
+          @rebuild = true
         end
         
         previous_revision_box_name = File.join @run_dir_boxes, "#{@project}-#{@revision_id.to_i - 1}_no_gems.pkg"
@@ -121,6 +113,19 @@ class Vigil
       @x._system "vagrant up"
       
       @x._system "vagrant ssh -c 'cd /vagrant; rake test'"
+    end
+
+    def _create_required_directories
+      @x.mkdir_p @run_dir_revision
+      @x.mkdir_p @run_dir_boxes
+    end
+
+    def _build_basebox
+      @x._system "vagrant basebox build --force --nogui '#{@project}'"
+      @x._system "vagrant basebox validate '#{@project}'"
+      @x._system "vagrant basebox export '#{@project}'"
+      @x._system "mv #{@project}.box #{@run_dir_boxes}/#{@project}-#{@revision_id}.box"
+      @x._system "vagrant basebox destroy #{@project}" #FIXME put in ensure block
     end
   end
 end
