b1885ea3b00f53530d1cb8f7273f5f4bfffb0fcb
Kjell-Magne Øierud <kjellm@oierud.net>
2013-04-14 09:03:59 +0200

    Use PTY for executing commands

--- lib/vigil/system.rb
+++ lib/vigil/system.rb
@@ -1,7 +1,32 @@
+require 'pty'
+
 class Vigil
   class System
     
     def run_command(command)
+      output = []
+      child = nil
+      p command
+      PTY.spawn(*command) do |r, w, pid|
+        child = pid
+        begin
+          # Do stuff with the output here. Just printing to show it works
+          r.each do |line|
+            break if line.nil? # Happens on BSD
+            print line
+            output << line
+          end
+        rescue Errno::EIO
+          # Happens on Linux
+        ensure
+          Process::wait(pid)
+        end
+      end
+      puts "#{child} #$?"
+      return CommandResult.new($?.exitstatus == 0, output.join(""), $?.clone)
+    end
+
+    def run_command2(command)
       output = ''
       IO.popen([*command, :err=>[:child, :out]]) do |io|
         output = io.read
