438e5c99939a2496cf4e84b4334490be098ee39c
Kjell-Magne Øierud <kjellm@oierud.net>
2013-04-14 12:48:37 +0200

    Add ruby class to manifest

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,3 +1,49 @@
+class ruby {
+  
+  $home = '/home/vagrant'
+  $user = 'vagrant'
+  $ruby = 'ruby-1.9.3-p392'
+  $rvm_version = 1.19.1
+  
+  Exec {
+    logoutput => true,
+    path      => ["$home/.rvm/bin", "/bin/", "/sbin/", "/usr/bin", "/usr/sbin"],
+    user        => $user,
+    environment => "HOME=$home",
+  }
+
+  # List taken from running 'rvm requirements'
+  $rvm_requirements = [ "bash", "curl", "git", "patch" ]                       
+  
+  package {
+    $rvm_requirements:
+      ensure => installed,
+  }
+  
+  exec {
+    "rvm":
+      command => "curl -s -L get.rvm.io | bash -s $rmv_version --autolibs=enabled",
+      require => Package[$rvm_requirements],
+      unless  => "test -d $home/.rvm",
+  }
+
+  exec {
+    "ruby":
+      command     => "bash -l -c 'rvm install $ruby'",
+      timeout     => 3600,
+      require     => Exec["rvm"],
+      unless      => "test -d $home/.rvm/rubies/$ruby/",
+  }
+  exec {
+    "use-ruby":
+      command     => "bash -l -c 'rvm use --default $ruby'",
+      require     => Exec["ruby"],
+      unless      => "bash -l -c 'ruby -v' | grep '1.9.3p392'", # FIXME
+  }
+}
+
+include ruby
+
 package { 
   "libzmq-dev":
     ensure => installed,
